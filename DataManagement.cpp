982911f3f0cda88f5cf795823a3fba0da90baba0
amibas <towsod@outlook.com>
2023-03-14 16:24:20 +0800

    数据管理

--- DataManagement.cpp
+++ DataManagement.cpp
@@ -1,3 +1,5 @@
 //
 // Created by amiba on 2023/3/14.
 //
+#include "DataManagement.h"
+
--- DataManagement.h
+++ DataManagement.h
@@ -5,4 +5,16 @@
 #ifndef SIMPLE_BOOK_MANAGEMENT_PROGRAM_DATAMANAGEMENT_H
 #define SIMPLE_BOOK_MANAGEMENT_PROGRAM_DATAMANAGEMENT_H
 
+#include "stdio.h"
+
+extern FILE *fp;    //文件指针
+extern char fname[100]; //文件名
+
+//书籍信息结构体
+typedef struct dateBook{
+    char bookName[80];  //书名
+    char bookNumber[10];    //图书编号
+
+}DATEU;
+
 #endif //SIMPLE_BOOK_MANAGEMENT_PROGRAM_DATAMANAGEMENT_H
